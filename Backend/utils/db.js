13cb87cb9d9a59dfddb2fcfeb260461324641727
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-09-02 16:07:00 +0300

    utils

---
+++ Backend/utils/db.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+class DBClient {
+  constructor() {
+    this.host = 'localhost';
+    this.port = '27017';
+    this.database = 'devchat';
+    try {
+      this.client = new mongoose.connect(
+        `mongodb://${this.host}:${this.port}/${this.database}`,
+        { useUnifiedTopology: true }
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  isAlive() {
+    return this.client.readyState;
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
