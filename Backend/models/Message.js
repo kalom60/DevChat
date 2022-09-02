79250385ee4c81be6e0e54f57eb59e8b7a331dcc
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-09-02 17:32:11 +0300

    messgae model

--- Backend/models/Message.js
+++ Backend/models/Message.js
@@ -27,6 +27,6 @@ const messageSchema = new mongoose.Schema({
   },
 });
 
-const Message = mongoose.model('User', messageSchema);
+const Message = mongoose.model('Message', messageSchema);
 
 export default Message;
