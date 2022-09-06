983a1dc4afe7c65d4e16ed022c992e74e255dbf1
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-09-06 15:07:13 +0300

    add cookie-parser

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import router from './routes';
 import dbClient from './utils/db';
@@ -8,8 +8,9 @@ import dbClient from './utils/db';
 const port = 5000;
 const app = express();
 
-app.use(bodyParser.json({ limit: '50mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(cookieParser());
+app.use(express.json({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
 app.use(router);
 
