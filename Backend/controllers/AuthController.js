50de69994ddd99827239ea3733f2852803310fdb
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-09-06 15:01:22 +0300

    add logout

--- Backend/controllers/AuthController.js
+++ Backend/controllers/AuthController.js
@@ -1,23 +1,30 @@
 require('dotenv').config();
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import GenerateToken from '../middlewares/generateToken';
 import User from '../models/User';
+import redisClient from '../utils/redis';
 
 class AuthController {
   static async login(req, res) {
     const { email, password } = req.body;
-
     const user = await User.findOne({ email });
-
     if (user === null) {
-      return res.status(404).json();
+      return res.status(404).json('No user');
     }
 
     try {
       if (await bcrypt.compare(password, user.password)) {
         const id = user._id.toString();
         const user_id = { id: id };
-        const token = jwt.sign(user_id, process.env.ACCESS_TOKEN_SECRET);
+        const token = await GenerateToken.generateAccessToken(user_id);
+        const refreshToken = await GenerateToken.generateRefreshToken(user_id);
+
+        await redisClient.set(refreshToken, id, 300);
+        res.cookie('jwt', refreshToken, {
+          httpOnly: true,
+          sameSite: 'lax',
+          maxAge: 5 * 60 * 1000,
+        });
         res.json({ token });
       } else {
         res.json('Wrong Password');
@@ -27,7 +34,24 @@ class AuthController {
     }
   }
 
-  static async logout(req, res) {}
+  static async logout(req, res) {
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(204);
+    const refToken = cookies.jwt;
+
+    const user = redisClient.get(refToken);
+    if (!user) {
+      res.clearCookie('jwt', {
+        httpOnly: true,
+        sameSite: 'lax',
+      });
+      res.sendStatus(204);
+    }
+
+    await redisClient.del(refToken);
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'lax' });
+    res.sendStatus(204);
+  }
 }
 
 export default AuthController;
