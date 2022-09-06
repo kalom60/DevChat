b960a03848db5cb2d89952da58ecb5a206477086
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-09-06 15:04:22 +0300

    add refresh route

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -4,34 +4,25 @@ import jwt from 'jsonwebtoken';
 import MessagesController from '../controllers/MessagesController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import AuthenticateToken from '../middlewares/auth';
+import RefreshTokenController from '../controllers/RefreshTokenController';
 
 const router = express.Router();
 
 // routes to usercontroller
 router.post('/register', UsersController.newUser);
-router.get('/me', UsersController.getMe);
-router.put('/edit', UsersController.editMe); // not yet made
-router.get('/dev/:id', UsersController.getDev);
+router.get('/me', AuthenticateToken.verifyToken, UsersController.getMe);
+router.put('/edit', AuthenticateToken.verifyToken, UsersController.editMe); // not yet made
+router.get('/dev/:id', AuthenticateToken.verifyToken, UsersController.getDev);
 
 // routes to messagecontroller
-router.post('/msg', MessagesController.newMsg);
-router.get('/msg', MessagesController.getMsg);
+router.post('/msg', AuthenticateToken.verifyToken, MessagesController.newMsg);
+router.get('/msg', AuthenticateToken.verifyToken, MessagesController.getMsg);
 
 // routes to AuthController
 router.post('/login', AuthController.login);
 router.get('/logout', AuthController.logout);
 
+router.get('/refresh', RefreshTokenController.handleRefreshToken);
+
 export default router;
-
-function authenticateToken(req, res, next) {
-  const authHeader = req.header('Authorization');
-  const token = authHeader && authHeader.split(' ')[1];
-  //   console.log(token);
-  if (token === null) return res.json('No token');
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user_id) => {
-    if (err) return res.json('Wrong token');
-    req.user = user_id;
-    next();
-  });
-}
