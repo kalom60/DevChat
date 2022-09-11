3882d27673dd704dae33f7aa0ba3dfae21006e62
mickiy123 <mikikinfe345@gmail.com>
2022-09-11 17:17:26 +0300

    update

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,26 +1,80 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState,useEffect } from 'react'
+import axios from '../axiosInstance'
 import styled from 'styled-components'
 import ChatInput from './ChatInput'
 import Logout from './Logout'
-import Messages from './Messages'
 
-function ChatContainer({currentChat}) {
+
+function ChatContainer({currentChat, currentUser}) {
+  const [messages, setMessages] = useState([])
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.post('/msg', {
+        from: currentUser._id,
+        to: currentChat._id,
+      })
+      console.log(response.data)
+      setMessages(response.data)
+    }
+    fetchData()
+  }, [currentChat])
+
   const handleSendMsg = async (msg) => {
-    alert(msg)
+    await axios.post('/msg', {
+      from: currentUser._id,
+      to: currentChat._id,
+      message: msg,
+    })
   }
   return (
     <Container>
-        <div className="chat-header">
-            <div className="user-details">
-                <div className="username">
-                    <h3>{currentChat.userName}</h3>
-                </div>
-            </div>
-            <Logout/>
+      <div className="chat-header">
+        <div className="user-details">
+          <div className="username">
+            <h3>{currentChat.username}</h3>
+          </div>
         </div>
-        <Messages />
-        <ChatInput handleSendMsg={handleSendMsg} />
+        <Logout />
+      </div>
+      <div className="chat-messages">
+        {messages.map((message) => {
+          return (
+            <div>
+              <div
+                className={`message ${
+                  message.fromSelf ? "sended" : "recieved"
+                }`}
+              >
+                <div className="content ">
+                  <p>{message.message}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <ChatInput handleSendMsg={handleSendMsg} />
     </Container>
+    // <Container>
+    //     <div className="chat-header">
+    //         <div className="user-details">
+    //             <div className="username">
+    //                 <h3>{currentChat.userName}</h3>
+    //             </div>
+    //         </div>
+    //         <Logout/>
+    //       <div className="chat-messages">
+    //         {
+    //           messgaes.map(message, {
+    //             return ()
+    //           })
+    //         }
+    //       </div>
+    //     </div>
+    //     <ChatInput handleSendMsg={handleSendMsg} />
+    // </Container>
   )
 }
 
