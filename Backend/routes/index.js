d443ca258a15b24918a60aba2b5df59ad24e3273
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-09-15 07:35:56 +0300

    add verify route

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -4,6 +4,7 @@ import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import AuthenticateToken from '../middlewares/auth';
 import RefreshTokenController from '../controllers/RefreshTokenController';
+import VerifyEmail from '../controllers/VerifyEmail';
 
 const router = express.Router();
 
@@ -11,8 +12,8 @@ const router = express.Router();
 router.post('/register', UsersController.newUser);
 router.get('/me', AuthenticateToken.verifyToken, UsersController.getMe);
 router.put('/edit', AuthenticateToken.verifyToken, UsersController.editMe); // not yet made
-router.get('/dev/:id', AuthenticateToken.verifyToken, UsersController.getDev);
-router.get('/users', AuthenticateToken.verifyToken, UsersController.allUsers);
+router.get('/dev', AuthenticateToken.verifyToken, UsersController.getDev);
+router.get('/users', UsersController.allUsers);
 
 // routes to messagecontroller
 router.post('/msg', AuthenticateToken.verifyToken, MessagesController.newMsg);
@@ -24,5 +25,8 @@ router.get('/logout', AuthController.logout);
 
 // route to refresh token
 router.get('/refresh', RefreshTokenController.handleRefreshToken);
+
+// route to verify email
+router.get('/user/verify-email', VerifyEmail.verify);
 
 export default router;
