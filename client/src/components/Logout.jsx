bfe2d8fe1eb2c993763fedf29a314f74fc0bc768
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-09-15 07:53:03 +0300

    logout component

--- client/src/components/Logout.jsx
+++ client/src/components/Logout.jsx
@@ -1,35 +1,34 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import {BiPowerOff} from 'react-icons/bi';
+import { BiLogOutCircle } from 'react-icons/bi';
 
 function Logout() {
-    const navigate = useNavigate()
-    const handleClick = async () => {
-        localStorage.clear();
-        navigate("/login")
-    }
+  const navigate = useNavigate();
+  const handleClick = async () => {
+    localStorage.clear();
+    navigate('/login');
+  };
   return (
-    <Button  onClick={handleClick}>
-      <BiPowerOff />
+    <Button onClick={handleClick}>
+      Logout <BiLogOutCircle />{' '}
     </Button>
-  )
+  );
 }
 
 const Button = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0.5rem;
-  border-radius: 0.5rem;
-  background-color: #9a86f3;
-  border: none;
+  // display: flex;
+  // justify-content: center;
+  // align-items: center;
+  // padding: 0.5rem;
+  // border-radius: 0.5rem;
+  // background-color: #9a86f3;
+  // border: none;
   cursor: pointer;
-  svg {
-    font-size: 1.3rem;
-    color: #ebe7ff;
-  }
-`
+  // svg {
+  //   font-size: 1.3rem;
+  //   color: #ebe7ff;
+  // }
+`;
 
-
-export default Logout
+export default Logout;
