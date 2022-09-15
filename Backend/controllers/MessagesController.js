ebe6da4a905996eab6415d20a69c007188cfe04b
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-09-15 07:32:44 +0300

    return message

--- Backend/controllers/MessagesController.js
+++ Backend/controllers/MessagesController.js
@@ -1,4 +1,6 @@
 import Message from '../models/Message';
+import User from '../models/User';
+import ObjectId from 'mongoose';
 
 class MessagesController {
   static async newMsg(req, res) {
@@ -8,21 +10,52 @@ class MessagesController {
     const newmsg = new Message(msg);
     console.log(newmsg);
     await newmsg.save();
+
+    const user = await User.findOne({ _id: sender });
+    console.log(user);
+    if (user) {
+      const reciver = user.chatRoom.filter((mem) => mem === conversation[1]);
+      if (reciver.length == 0) {
+        user.chatRoom.push(conversation[1]);
+        await user.save();
+      } else {
+        console.log('already found', reciver);
+      }
+    } else {
+      console.log('no user');
+    }
     return res.json(newmsg);
   }
 
   static async getMsg(req, res) {
-    const { user_id, dev_id } = req.body;
+    const { dev } = req.query;
+    const user = req.user.id;
 
-    if (user_id !== null && dev_id !== null) {
-      const convo = await Message.find({ conversation: [user_id, dev_id] });
+    if (!dev) {
+      console.log('no ids');
+      return;
+    }
+
+    if (dev !== null) {
+      const convo = await Message.find({ conversation: [user, dev] });
+      const convo1 = await Message.find({ conversation: [dev, user] });
       if (!convo) return res.json('No conversation');
       // console.log(convo);
 
       const { text, sender } = convo[0];
       const receiver = convo[0].conversation[1];
 
-      return res.json({ text, sender, receiver });
+      const from = { text, sender, receiver };
+      let to = {};
+
+      if (convo1.length > 0) {
+        const text1 = convo1[0].text;
+        const sender1 = convo1[0].sender;
+        const receiver1 = convo1[0].conversation[1].receiver;
+        to = { text1, sender1, receiver1 };
+      }
+
+      return res.json({ from, to });
     }
   }
 }
